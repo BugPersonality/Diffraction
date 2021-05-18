f602009d3e9d3dd08f3c78aa5b12cb21eb7330cd
BugPersonality <dubov.danil.01@mail.ru>
2021-05-18 16:47:18 +0300

    add jopa.py

---
+++ jopa.py
@@ -0,0 +1,33 @@
+import numpy as np 
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import math
+
+constPlank = 1.054 * 10 ** (-34)    # Дж / c
+deltaX = 5.5 * 10 ** (-10)          # м 
+deltaPhi = 50                       # В 
+massE = 9.1093837015 * 10 ** (-31)  # кг
+e = 1.60217662 * 10 ** (-19)        # Кл
+
+def getPx(k):
+    return ((2 * k - 1) * math.pi * 2 * constPlank) / (12 * deltaX)
+
+def getPhiX(px, p):
+    return np.arcsin(px / p)
+
+def getP():
+    return math.sqrt(e * deltaPhi * massE * 2)
+
+arrK = [i for i in range(1, 11)]
+
+arrPxk = []
+arrPhiK = []
+
+for k in arrK:
+    pxk = getPx(k)
+    phixK = getPhiX(pxk, getP())
+    arrPxk.append(pxk)
+    arrPhiK.append(phixK)
+
+print(f"Имульсы сука = \n {arrPxk} \n")
+print(f"Углы сука = \n {arrPhiK} \n")
