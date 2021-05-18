2ee007cdc7e5b42945c398793d43f84d0b220e89
Alexey Gorbunov <alexeygorbunov72@gmail.com>
2021-05-18 16:43:03 +0300

    add distribution

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import random
 import numpy as np 
 from scipy.stats import norm
+import math
 import matplotlib.pyplot as plt
 
-def draw_random_number_from_pdf(pdf, interval, pdfmax = 1, max_iterations = 100):
-        for i in range(max_iterations):
-            rand_x = (interval[1] - interval[0]) * np.random.random(1) + interval[0] 
+def getDistribution(listP, amount):
+    class Section:
+        start = 0
+        end = 0
+        value = 0
+        def __init__(self, start, end):
+            self.start = start
+            self.end = end
 
-            rand_y = pdfmax * np.random.random(1) 
-            calc_y = pdf(rand_x)
+    dx = 0.55 * 10 ** (-34)
+    h = 1.054 * 10 ** (-34)
+    phi = lambda p: sqrt(dx / (2 * math.pi * h) * ((sin(p * dx/ (2 * h)) )) / ((p * dx) / (2 * h) ))
+    probabilityP = []
+    for p in listP:
+        probabilityP.append(phi(p))
+    sum_ = sum(probabilityP)
+    for p in probabilityP:
+        p /= sum_
+    start = 0
+    probabilitySection = []
+    for p, val in zip(probabilityP, listP):
+        section = Section(start, start + p)
+        section.value = val
+        start += p
+        probabilitySection.append(section)
 
-            if(rand_y <= calc_y ):
-                return rand_x
+    distribution = []
+    for _ in range(n):
+        randVal = random.uniform(0, 1)
+        for section in probabilitySection:
+            if  section.start <= randVal <= section.end:
+                distribution.append(section.value)
+                break
+    return distribution
+
+
 
 arr = []
 x_axis = np.arange(0, 1, 0.1)
