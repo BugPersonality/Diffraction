8f9ec0b344ed65c5e012f5fea22cf03027c62826
BugPersonality <dubov.danil.01@mail.ru>
2021-05-19 20:28:42 +0300

    Papkov help pls

--- animation.cpp
+++ animation.cpp
@@ -5,23 +5,175 @@
 #include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
+#include<vector>
 
 using namespace std;
-int main(){
-    int w = 700; int h = 700;
-    int n = 70;
-    srand (time(NULL));
-    sf::RenderWindow window(sf::VideoMode(w, h), "Diffraction");    
-    while (window.isOpen())
-    {        
+using namespace sf;
+
+// double getDistanceBetweenTwoPoints(Point point1, Point point2){
+//     return sqrt(pow(point1.x - point2.x, 2) + pow(point1.y - point2.y, 2));
+// }
+
+class Back_Groung {
+    public:
+    Texture texture;
+    Sprite sprite;
+
+    Back_Groung(String F)
+    {
+        texture.loadFromFile(F);
+        Vector2u size = texture.getSize();
+        sprite.setTexture(texture);
+        sprite.setPosition(400, 400);
+        sprite.setOrigin(size.x / 2, size.y / 2);
+    }
+};
+
+class Receiver {
+    public:
+
+    float x, y, w, h;
+
+    String file;
+    Image image;
+    Texture texture;
+    Sprite sprite;
+
+    Receiver(String F, float X, float Y, float W, float H)
+	{
+        file = F;
+        w = W; h = H;
+        x = X;
+        y = Y;
+
+        image.loadFromFile(file);
+
+        image.createMaskFromColor(Color(0, 0, 0));
+        texture.loadFromImage(image);
+        sprite.setTexture(texture);
+
+        sprite.setPosition(x, y);
+        sprite.setOrigin(w / 2, h / 2);
+	}
+
+    FloatRect getRect() 
+    {
+        return sprite.getGlobalBounds();
+    }
+};
+
+class Hole {
+    public:
+    Texture texture;
+    Sprite sprite;
+
+    Hole(String F)
+    {
+        texture.loadFromFile(F);
+        Vector2u size = texture.getSize();
+        sprite.setTexture(texture);
+        sprite.setPosition(360, 388);
+        sprite.setOrigin(size.x / 2, size.y / 2);
+    }
+
+    FloatRect getRect() 
+    {
+        return sprite.getGlobalBounds();
+    }
+};
+
+class Electron{
+    public:
+    
+    float x;
+    float y;
+    float speed;
+
+    float dx, dy;
+
+    CircleShape point;
+
+    Electron() {
+        point = CircleShape(5.f);
+        point.setFillColor(Color::Green);
+        x = 75;
+        y = 388;
+        speed = 0.1;
+        dx = speed;
+        dy = 0;
+        point.setPosition(x, y);
+    }
+
+    void update(float time) {
+        x += (dx * time);
+        y += dy * time;              
+       
+        point.setPosition(x, y);
+    }
+
+    FloatRect getRect() 
+    {
+        return point.getGlobalBounds();
+    }
+};
+
+
+int main() {
+    Clock clock;
+    
+    RenderWindow window(VideoMode(800, 800), "BrownianMotion");
+
+    vector<Electron> points;
+    vector<int> keys; 
+    
+    for(int i = 0; i < 1; i++) {
+        points.push_back(Electron());
+    }
+
+    ifstream infile("resources/keys.txt");
+    
+    int count, countOFKeys;
+    infile >> count >> countOFKeys;
+    
+    for (int i = 0; i < count; ++i) {
+        int key;
+        infile >> key;
+        keys.push_back(key);
+    }
+
+    infile.close();
+
+    // for (auto& key : keys)
+    //     cout << key << endl;
+
+    Back_Groung background("resources/imgs/labTable.jpg");
+    Hole hole("resources/imgs/hole.png");
+
+    while (window.isOpen()) {
+        // Events 
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }  
         }
 
+        // Calculations
+        float time = clock.getElapsedTime().asMicroseconds();
+        clock.restart();
+        time = time / 800;
+
+        points[0].update(time);
+
+        if (hole.getRect().intersects(points[0].getRect())) {
+            points[0].dx = 0;
+        }
+
+        // Drawing 
         window.clear();
+        window.draw(hole.sprite);
+        window.draw(background.sprite);
+        window.draw(points[0].point);
         window.display();
     }
 
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import numpy as np 
+import numpy as np
 from scipy.stats import norm
+import scipy.stats as st
 import matplotlib.pyplot as plt
 import math
-import random
+import random 
 
 constPlank = 1.054 * (10 ** (-34))      # Дж / c
 deltaX = 5.5 * (10 ** (-10))            # м 
@@ -10,11 +11,26 @@ deltaPhi = 50                           # В
 massE = 9.1093837015 * (10 ** (-31))    # кг
 e = 1.60217662 * (10 ** (-19))          # Кл
 
+def getPx(k):
+    return ((2 * k - 1) * math.pi * 2 * constPlank) / (12 * deltaX)
+
+def getPhiX(px, p):
+    return np.arcsin(px / p)
+
+def getP():
+    return math.sqrt(e * deltaPhi * massE * 2)
+
 def waveFunction(px):
     return math.sqrt(deltaX / (2 * math.pi * constPlank)) * (math.sin((px * deltaX) / (2 * constPlank)) / ((px * deltaX) / (2 * constPlank)))
 
 def waveFunctionAbsPow(px):
     return math.pow(abs(waveFunction(px)), 2)
+
+def getDeBroglieWavelength(p, mode):
+    if mode == "angle":
+        return deltaX * math.sin(p)
+    elif mode == "pulse":
+        return (2 * constPlank) / p
 
 def getDistribution(listP, amount):
     class Section:
@@ -27,13 +43,11 @@ def getDistribution(listP, amount):
 
     dx = 0.55 * 10 ** (-34)
     h = 1.054 * 10 ** (-34)
-    phi = waveFunctionAbsPow #lambda p: pow(abs(math.sqrt(dx / (2 * math.pi * h) * ((math.sin(p * dx/ (2 * h)) )) / ((p * dx) / (2 * h) ))), 2)
+    phi = waveFunctionAbsPow 
     probabilityP = []
     for p in listP:
         probabilityP.append(phi(p))
     sum_ = sum(probabilityP)
-    # for p in probabilityP:
-    #     p /= sum_
     for p in range(len(probabilityP)):
         probabilityP[p] /= sum_
     start = 0
@@ -53,3 +67,84 @@ def getDistribution(listP, amount):
                 break
    
     return distribution
+
+arrK = [i for i in range(1, 11)]
+
+arrPxk = []
+arrPhiK = []
+dictKToPx = {}
+dictKToPhi = {}
+
+for k in arrK:
+    pxk = getPx(k)
+    dictKToPx[k] = pxk
+    phixK = getPhiX(pxk, getP())
+    dictKToPhi[k] = phixK
+    arrPxk.append(pxk)
+    arrPhiK.append(phixK)
+
+dist = np.array(getDistribution(arrPxk, 100))
+x = list(dist)
+x.sort()
+dict = {}
+
+for x_ in x:
+    if dict.get(x_) is not None:
+        dict[x_] += 1
+    else:
+        dict[x_] = 1
+
+arrCountPxForK = []
+
+for key in range(1, 11):
+    if dict.get(dictKToPx[key]) is not None:
+        arrCountPxForK.append(dict[dictKToPx[key]]/dict[dictKToPx[1]])
+    else:
+        arrCountPxForK.append(0)
+
+arrForMainCpp = []
+
+for px in x:
+    for key in range(1, 11):
+        if px == dictKToPx[key]:
+            arrForMainCpp.append(key)
+            break
+
+random.shuffle(arrForMainCpp)
+
+f = open('resources/keys.txt', 'w')
+f.write(f"{len(arrForMainCpp)} {len(dictKToPx)} \n")
+for i in arrForMainCpp:
+    f.write(str(i) + " \n")
+f.close()
+
+# barlist = plt.bar(arrK, arrCountPxForK)
+# firstMin = 1
+
+# for i in range(1, len(arrCountPxForK) - 1):
+#     if (arrCountPxForK[i] < arrCountPxForK[i - 1]) and (arrCountPxForK[i] <= arrCountPxForK[i + 1]):
+#         barlist[i].set_color('r')
+#         firstMin = (i, arrCountPxForK[i])
+#         break
+#     elif (arrCountPxForK[i] < arrCountPxForK[i - 1]) and (arrCountPxForK[i] < arrCountPxForK[i + 1]):
+#         barlist[i].set_color('r')
+#         firstMin = (i, arrCountPxForK[i])
+#         break
+
+# plt.show()
+
+# deBroglieWavelengthByPulse = getDeBroglieWavelength(firstMin[1], "pulse")
+# deBroglieWavelengthByAngle = getDeBroglieWavelength(dictKToPhi[firstMin[0]], "angle")
+
+# print(f"De Broglie wavelength by angle and pulse: {deBroglieWavelengthByAngle} ?= {deBroglieWavelengthByPulse}")
+
+
+# arrWaveFunc = []
+# arrYForWaveFunc = []
+
+# for px in dist:
+#     arrWaveFunc.append(waveFunctionAbsPow(px))
+#     arrYForWaveFunc.append((px * deltaX) / (2 * constPlank))
+
+# plt.scatter(arrWaveFunc, arrYForWaveFunc)
+# plt.show()
